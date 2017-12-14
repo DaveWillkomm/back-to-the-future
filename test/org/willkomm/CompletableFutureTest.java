34e37765ad1237d19712a9526ee701af55a00b3a
Dave Willkomm <david.willkomm@imanage.com>
2017-12-14 16:14:45 -0600

    wip

--- test/org/willkomm/CompletableFutureTest.java
+++ test/org/willkomm/CompletableFutureTest.java
@@ -3,16 +3,26 @@ package org.willkomm;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CompletableFutureTest {
     public class Fubar extends RuntimeException {}
 
+    public String goOrFail(boolean go) {
+        if (go) {
+            return "Let's go!";
+        }
+        throw new Fubar();
+    }
+
     @Test
     public void intermediateStageHandlesException_givenNoException() {
         CompletableFuture<String> f = CompletableFuture.supplyAsync(
-                () -> "lets go!"
+                () -> goOrFail(true)
         ).handleAsync(
                 (result, reason) -> {
                     if (reason instanceof Fubar) {
@@ -23,19 +33,13 @@ public class CompletableFutureTest {
                 }
         );
 
-        assertEquals("lets go!", f.join());
+        assertEquals("Let's go!", f.join());
     }
 
     @Test
     public void intermediateStageHandlesException_givenAnException() {
         CompletableFuture<String> f = CompletableFuture.supplyAsync(
-                () -> {
-                    if (false) {
-                        return "lets go!";
-                    } else {
-                        throw new Fubar();
-                    }
-                }
+                () -> goOrFail(false)
         ).handleAsync(
                 (result, reason) -> {
                     if (reason.getCause() instanceof Fubar) {
@@ -47,5 +51,29 @@ public class CompletableFutureTest {
         );
 
         assertEquals("boo", f.join());
+    }
+
+    @Test
+    public void intermediateStageIgnoresException_givenNoException() {
+        CompletableFuture<String> f = CompletableFuture.supplyAsync(
+                () -> goOrFail(true)
+        ).thenApplyAsync(
+                (result) -> result
+        );
+
+        assertEquals("Let's go!", f.join());
+    }
+
+    @Test
+    public void intermediateStageIgnoresException_givenAnException() {
+        Throwable t = assertThrows(
+                ExecutionException.class,
+                () -> CompletableFuture.supplyAsync(
+                        () -> goOrFail(false)
+                ).thenApplyAsync(
+                        (result) -> result
+                ).get()
+        );
+        assertEquals(t.getCause().getClass(), Fubar.class);
     }
 }
